0dcbbe3c83c8cfee24cf8860f04436fe51bec3b5
mizuno <31834@toyota.kosen-ac.jp>
2022-11-21 02:16:11 +0900

    set up server

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
@@ -14,7 +15,13 @@ func nowTime() {
 	fmt.Println("The time is", time.Now())
 }
 
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
+
 func main() {
-	hello()
-	nowTime()
+	// hello()
+	// nowTime()
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
