25889d1fb990cb310664b810f5dc9289acc21421
mizuno <31834@toyota.kosen-ac.jp>
2022-11-22 15:43:31 +0900

    add rand function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
 	"time"
 )
@@ -19,9 +20,14 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
 }
 
+func rand() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
+
 func main() {
 	// hello()
 	// nowTime()
+	rand()
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
